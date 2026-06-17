084ce2823e2cce6688e425d71f0d2625a195c7e7
Andrea D'Amico <andrea.damico@gmail.com>
2026-06-17 17:30:12 +0200

    chore: whitespace

--- include/enemy_swarm.hpp
+++ include/enemy_swarm.hpp
@@ -8,15 +8,15 @@
 
 class EnemySwarm {
 public:
-    EnemySwarm() {};
-    void Init(Vector2 startPos);
-    void Update(Vector2 dir, float speed, float dt);
-    void Draw(struct GameState* state);
-    void HandleCollisions(struct GameState* state);
+  EnemySwarm() {};
+  void Init(Vector2 startPos);
+  void Update(Vector2 dir, float speed, float dt);
+  void Draw(struct GameState *state);
+  void HandleCollisions(struct GameState *state);
 
-    Enemy enemies[MAX_ENEMIES];
-    Vector2 direction;
-    int speed;
-    bool needToMoveDown;
-    int activeCount;
+  Enemy enemies[MAX_ENEMIES];
+  Vector2 direction;
+  int speed;
+  bool needToMoveDown;
+  int activeCount;
 };
